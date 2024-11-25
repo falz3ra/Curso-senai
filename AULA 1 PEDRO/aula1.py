3cbacad1664b2aa46bed49896dd9008f112d0ee5
falz3ra <pedrohffb2006@gmail.com>
2024-11-25 15:49:12 -0300

    Pasta aula 1 de python

---
+++ AULA 1 PEDRO/aula1.py
@@ -0,0 +1,12 @@
+print("olá, mundo")
+print("rio de hiroshima")
+print("presidente")
+print("irmãos separados")
+meu_nome = 'Pedro'
+meu_sobrenome = "Fernandes"
+print("meu nome é:",meu_nome)
+
+meu_nome = input()
+comida_favorita = input()
+
+print("meu nome é:",meu_nome,"e gosto de:",comida_favorita)
